a87bad40d17209b89786c76289db20ac477dd2c1
Rainer <13830117+rainco77@users.noreply.github.com>
2025-12-10 16:24:45 +0100

    tidy up [skip ci]

--- src/archive/2025-09-24/main/java/org/evochora/server/contracts/IQueueMessage.java
+++
@@ -1,9 +0,0 @@
-package org.evochora.server.contracts;
-
-/**
- * Marker interface for messages exchanged between the DummyService and the PersistenceService.
- */
-public interface IQueueMessage {
-}
-
-
