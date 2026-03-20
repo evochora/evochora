dffd361fef654a52a1e274231cd5654e8270b1df
Rainer <13830117+rainco77@users.noreply.github.com>
2026-03-20 22:16:54 +0100

    removed unnecessary import

--- src/test/java/org/evochora/compiler/model/symbols/SymbolTableFreezeTest.java
+++ src/test/java/org/evochora/compiler/model/symbols/SymbolTableFreezeTest.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.Map;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
